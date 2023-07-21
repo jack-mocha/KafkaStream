7df46e8a1237115fa1392971ec57071dbd24aa7b
Jack Hsu <jack.h.mocha@gmail.com>
2023-07-20 22:57:50 -0400

    Build a producer

--- KafkaStream.Producer/Program.cs
+++ KafkaStream.Producer/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+	BootstrapServers = "localhost:9092",
+	ClientId = "producer-1"
+};
+
+using (var producer = new ProducerBuilder<string, string>(config).Build())
+{
+	var message = new Message<string, string>
+	{
+		Key = null, // Set the key if you want to partition the messages
+		Value = "Hello, Kafka!"
+	};
+
+	try
+	{
+		var deliveryResult = await producer.ProduceAsync("input-topic", message);
+		Console.WriteLine($"Message delivered to {deliveryResult.TopicPartitionOffset}");
+	}
+	catch (ProduceException<string, string> ex)
+	{
+		Console.WriteLine($"Delivery failed: {ex.Error.Reason}");
+	}
+}
